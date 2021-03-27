be230c9125d5ba2e411f0a54540d8edc3e453b0e
hyeweon <berry_na@khu.ac.kr>
2021-03-27 23:16:10 +0900

    baekjoon

---
+++ 210327/1316.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	int count = N;
+	string word;
+
+	for (int i = 0; i < N; i++) {
+		cin >> word;
+		int alphabet[26] = { 0, };
+		for (int i = 0; i < word.size(); i++) {
+			int index = word[i] - 97;
+			if ((alphabet[index] != 0) && (i - alphabet[index] > 0)) {
+				count--;
+				break;
+			}
+			else
+				alphabet[index] = i + 1;
+		}
+	}
+
+	cout << count << endl;
+
+	return 0;
+}
---
+++ 210327/4153.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y, z;
+	while (true) {
+		cin >> x >> y >> z;
+		if (x == 0)
+			break;
+
+		if (x > y && x > z) {
+			if (x * x == y * y + z * z)
+				cout << "right";
+			else
+				cout << "wrong";
+		}
+		else if (z > y) {
+			if (z * z == x * x + y * y)
+				cout << "right";
+			else
+				cout << "wrong";
+		}
+		else {
+			if (y * y == x * x + z * z)
+				cout << "right";
+			else
+				cout << "wrong";
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
