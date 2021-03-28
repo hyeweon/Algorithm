2257dfac88152c8251e8bc5740c760753130e603
hyeweon <berry_na@khu.ac.kr>
2021-03-28 17:51:54 +0900

    baekjoon

---
+++ 210328/1002.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		int x1, y1, r1, x2, y2, r2;
+		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+
+		int d = (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
+		int r = (r1 + r2)*(r1 + r2);
+		int r_ = (r1 - r2)*(r1 - r2);
+
+		if (d == 0) {
+			if (r1 == r2)
+				cout << -1;
+			else
+				cout << 0;
+		}
+		else {
+			if (d == r || d == r_)
+				cout << 1;
+			else if (d > r)
+				cout << 0;
+			else if (d > r_)
+				cout << 2;
+			else
+				cout << 0;
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
---
+++ 210328/3053.cpp
@@ -0,0 +1,17 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double r; //*자료형
+	cin >> r;
+
+	cout << fixed;
+	cout.precision(6);
+	cout << r * r * M_PI << endl;
+	cout << (r * 2)*r << endl;
+
+	return 0;
+}
