e7d22233ae713cdfeb8a2f514156674c86c8482c
hyeweon <berry_na@khu.ac.kr>
2021-03-29 17:48:52 +0900

    beakjoon

---
+++ 210329/10870.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonacci(int x) {
+	if (x < 2)
+		return x;
+	else
+		return fibonacci(x - 1) + fibonacci(x - 2);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << fibonacci(n) << endl;
+
+	return 0;
+}
---
+++ 210329/10872.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int factorial(int x) {
+	if (x == 0)
+		return 1;
+	else
+		return factorial(x - 1) * x;
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	cout << factorial(N) << endl;
+
+	return 0;
+}
