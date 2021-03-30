960e0c9e992d6e3c77773a3781218e1b2307c9e3
hyeweon <berry_na@khu.ac.kr>
2021-03-31 00:00:25 +0900

    baekjoon

---
+++ 210330/7568.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+struct body {
+	int weight;
+	int height;
+	int num = 1;
+};
+
+int main()
+{
+	int N;
+	cin >> N;
+	body *Body = new body[N];
+
+	for (int i = 0; i < N; i++) {
+		cin >> Body[i].weight >> Body[i].height;
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if ((Body[i].weight < Body[j].weight) && (Body[i].height < Body[j].height))
+				Body[i].num++;
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		cout << Body[i].num << ' ';
+	}
+
+	return 0;
+}
