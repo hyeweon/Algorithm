e79574aa4ed33f52670c31e56e9b4ad255618034
hyeweon <berry_na@khu.ac.kr>
2021-04-01 23:52:02 +0900

    baekjoon

---
+++ 210401/10757.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string A, B;
+	cin >> A >> B;
+
+	int Alength = A.length();
+	int Blength = B.length();
+	if (Alength < Blength) {
+		for (int i = 0; i < (Blength - Alength); i++) {
+			A = '0' + A;
+		}
+	}
+	else if (Alength > Blength) {
+		for (int i = 0; i < (Alength - Blength); i++) {
+			B = '0' + B;
+		}
+	}
+
+	string sum;
+	char temp;
+	char carry = 0;
+
+	for (int i = (A.size() - 1); i >= 0; i--) {
+		temp = A[i] + B[i] + carry - 48;
+		if (temp > '9') {
+			temp = temp - 10;
+			carry = 1;
+			sum = temp + sum;
+		}
+		else {
+			carry = 0;
+			sum = temp + sum;
+		}
+	}
+
+	if (carry == 1)
+		sum = '1' + sum;
+
+	cout << sum;
+
+	return 0;
+}
