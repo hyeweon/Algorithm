d92a5f3bfbf3323d9458571ddf0449403d5f0550
hyeweon <berry_na@khu.ac.kr>
2021-04-02 21:39:17 +0900

    beakjoon

---
+++ 210402/1011.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+	
+	for (int i = 0; i < T; i++) {
+		double y, x;
+		cin >> x >> y;
+
+		double count = 0;
+		double d = (y - x) / 2;
+		double k = 1;
+		//*자료형
+		for (; d > k; k++) {
+			d = d - k;
+			count++;
+		}
+
+		count = count * 2;
+		if (d > k / 2)
+			count += 2;
+		else if (d != 0)
+			count++;
+
+		cout << count << endl;
+	}
+
+	return 0;
+}
