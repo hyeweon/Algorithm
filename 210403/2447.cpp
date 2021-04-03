77bc633d647ee0d1fc0b01c3cd5b47718f40a810
hyeweon <berry_na@khu.ac.kr>
2021-04-03 18:06:59 +0900

    beakjoon

---
+++ 210403/2447.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string *stars(int N) {
+	string *lines = new string[N];
+
+	if (N == 3) {
+		lines[0] = "***";
+		lines[1] = "* *";
+		lines[2] = "***";
+	}
+	else {
+		int pre = N / 3;
+		string *lines_ = stars(pre);
+
+		string blanks = " ";
+		for (int i = 1; i < N / 3; i++) {
+			blanks = blanks + ' ';
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (i >= pre && i < pre * 2)
+				lines[i] = lines_[i % pre] + blanks + lines_[i % pre];
+			else
+				lines[i] = lines_[i % pre] + lines_[i % pre] + lines_[i % pre];
+		}
+	}
+
+	return lines;
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+	string *lines = stars(N);
+
+	for (int i = 0; i < N; i++) {
+		cout << lines[i] << endl;
+	}
+
+	return 0;
+}
