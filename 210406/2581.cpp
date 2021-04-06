8a5b81662619da16c9d4dccb5ed4d42817a10266
hyeweon <berry_na@khu.ac.kr>
2021-04-06 23:49:47 +0900

    baekjoon

---
+++ 210406/2581.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	bool self = true;
+
+	for (; N > 1; self = true) {
+		for (int i = 2; i < N; i++) {
+			if (N%i == 0) {
+				N = N / i;
+				self = false;
+				cout << i << endl;
+				break;
+			}
+		}
+		if (self == true) {
+			cout << N << endl;
+			break;
+		}
+	}
+
+	return 0;
+}
