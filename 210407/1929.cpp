6a0dc71a5407bbf2804a81cd40b7bfd1f796402f
hyeweon <berry_na@khu.ac.kr>
2021-04-07 17:24:41 +0900

    baekjoon

---
+++ 210407/1929.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int M, N;
+	cin >> M >> N;
+	bool *prime = new bool[N - M + 1];
+	prime = { ,true };
+
+	for (int i = 2; i <= (N - M); i++) {
+		if (prime[i] == )
+			continue;
+		else
+			for (int j = i + i; j <= (N - M); j += i) {
+				num[j] = 0;
+			}
+	}
+
+	return 0;
+}
