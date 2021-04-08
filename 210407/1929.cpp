1fcc6e8ef66ee2ab21ca5b0f3d49c8d8e76b63f9
hyeweon <berry_na@khu.ac.kr>
2021-04-08 23:26:19 +0900

    baekjoon

--- 210407/1929.cpp
+++ 210407/1929.cpp
@@ -1,22 +1,24 @@
 ﻿#include <iostream>
-#include <math.h>
 using namespace std;
 
 int main()
 {
 	int M, N;
 	cin >> M >> N;
-	bool *prime = new bool[N - M + 1];
-	prime = { ,true };
+	bool *prime = new bool[N + 1]();
 
-	for (int i = 2; i <= (N - M); i++) {
-		if (prime[i] == )
+	for (int i = 2; i <= N; i++) {
+		if (prime[i] == 0)
 			continue;
 		else
-			for (int j = i + i; j <= (N - M); j += i) {
-				num[j] = 0;
+			for (int j = i + i; j <= N; j += i) {
+				prime[j] = 1;
 			}
 	}
-
+	for (int i = M; i <= N; i++) {
+		if (prime[i] == 0)
+			cout << i << endl;
+	}
+	
 	return 0;
 }
