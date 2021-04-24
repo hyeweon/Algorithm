c333d34a1f0484b6b58fd05d1800d87f4343bae3
hyeweon <56705426+hyeweon@users.noreply.github.com>
2021-04-24 23:48:56 +0900

    baekjoon

---
+++ 210424/2562.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n[9];
+    for (int i = 0; i < 9; i++) {
+        cin >> n[i];
+    }
+
+    int max = n[0];
+    int index = 0;
+    for (int i = 1; i < 9; i++) {
+        if (max < n[i]) {
+            max = n[i];
+            index = i;
+        }
+    }
+    cout << max << endl;
+    cout << index + 1 << endl;
+
+    return 0;
+}
