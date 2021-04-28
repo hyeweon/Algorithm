286c6ba850fe3e095c8f30897d542aa7eefb64df
hyeweon <berry_na@khu.ac.kr>
2021-04-28 23:54:18 +0900

    baekjoon

--- 210425/10818.cpp
+++ 210425/10818.cpp
@@ -4,9 +4,20 @@ using namespace std;
 int main()
 {
 	int N;
-	for (int i = 0; i < N; i++) {
+	cin >> N;
 
+	int *num = new int[N];
+	int min = 1000001;
+	int max = -1000000;
+
+	for (int i = 0; i < N; i++) {
+		cin >> num[i];
+		if (num[i] > max)
+			max = num[i];
+		if (num[i] < min)
+			min = num[i];
 	}
 	
+	cout << min << ' ' << max << endl;
 	return 0;
 }
